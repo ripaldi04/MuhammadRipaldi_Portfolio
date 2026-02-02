6422ceab648250d2f71c42fb5053e01be7b6b4fc
Muhammad Ripaldi <muhamadripaldi75@gmail.com>
2026-02-02 12:47:43 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
